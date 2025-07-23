a234a6a215541781cafa82bfb8af8b4ee3c94f13
小朱 <10714957+xiao-zhu245@user.noreply.gitee.com>
2025-07-23 22:26:28 +0800

    增加配置文件说明

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -6,7 +6,7 @@ export default navbar([
     text: "部署",
     icon: "fa-download",
     prefix: "/部署/",
-    children: ["Windows","Linux","Docker"]
+    children: ["Windows","Linux","Docker","面板配置文件"]
   },
   {
     text: "功能详解",
